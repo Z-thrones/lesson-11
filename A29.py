42228871c35d64e7c73624c784e7c220b2a8c0fc
Z-thrones <zaviyar.ktk@gmail.com>
2026-04-06 18:10:03 +0500

    Add files via upload

---
+++ A29.py
@@ -0,0 +1,13 @@
+choise = int(input("Enter a ride, 1 for bike and 2 for car: "))
+if choise == 1:
+    choise2 = int(input("Enter a type of bike,scooty or scooter. 1 for scooty"))
+    if choise2 == 1:
+        print("You have selected scooty")
+    else:
+        print("You have selected scooter")
+else:
+    choise22 = int(input("Enter a type of bike , sedan or SUV , 1 for sedan: "))
+    if choise22 == 1:
+        print("You have selected Sedan")
+    else:
+        print("You have selected SUV")
